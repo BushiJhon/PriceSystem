5c30ed85277a38d5fffcacc601348ffe217f7f4a
Kind-Jhon <1172905107@qq.com>
2021-03-18 15:11:54 +0800

    zuul网关

--- services/user-information-management/src/main/java/com/spike/userinformationmanagement/controller/InformationController.java
+++ services/user-information-management/src/main/java/com/spike/userinformationmanagement/controller/InformationController.java
@@ -4,10 +4,7 @@ import com.spike.userinformationmanagement.mapper.InformationMapper;
 import com.spike.userinformationmanagement.pojo.Token;
 import com.spike.userinformationmanagement.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class InformationController {
@@ -15,9 +12,18 @@ public class InformationController {
     private InformationMapper informationMapper;
 
     //查询用户信息
-    @RequestMapping(value = "/user/showInformation", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
-    public User showInformation(@RequestBody Token token){
-        User user = informationMapper.selectUser(token.getId());
+    @RequestMapping(value = "/user/showInformation", produces = "application/json", method = RequestMethod.GET)
+    public User showInformation(@RequestHeader(value = "Authorization") String str){
+        System.out.println(str);
+        User user = informationMapper.selectUser(1);
         return user;
+    }
+
+    //修改用户信息
+    @RequestMapping(value = "/user/modifyInformation", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
+    public Boolean modifyInformation(@RequestHeader(value = "Authorization") String authorization, @RequestBody User user){
+        System.out.println(authorization);
+        Boolean result = informationMapper.updateUser("云南省", "昆明市", "电缆", "行业介绍", "电力", 1);
+        return result;
     }
 }
--- services/user-information-management/src/main/java/com/spike/userinformationmanagement/mapper/InformationMapper.java
+++ services/user-information-management/src/main/java/com/spike/userinformationmanagement/mapper/InformationMapper.java
@@ -3,6 +3,7 @@ package com.spike.userinformationmanagement.mapper;
 import com.spike.userinformationmanagement.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,4 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface InformationMapper {
     @Select("select * from user where id = #{id}")
     User selectUser(Integer id);
+
+    @Update("update user set province = #{province}, city = #{city}, company = #{company}, introduction = #{introduction}, industry = #{industry}")
+    Boolean updateUser(String province, String city, String company, String introduction, String industry, Integer id);
 }

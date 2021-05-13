d4fc6dcf8fcce568ba2279b8a8909801bdfb8c99
BushiJhon <1172905107@qq.com>
2021-05-13 14:22:14 +0800

    修改配置文件

--- api/hystrix-dashboard/src/main/java/com/spike/hystrixdashboard/HystrixDashboardApplication.java
+++ api/hystrix-dashboard/src/main/java/com/spike/hystrixdashboard/HystrixDashboardApplication.java
@@ -2,6 +2,7 @@ package com.spike.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableHystrixDashboard
